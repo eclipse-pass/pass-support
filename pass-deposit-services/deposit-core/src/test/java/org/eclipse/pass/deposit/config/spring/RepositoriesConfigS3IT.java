426d3e6173a7b267513d94deb765e3534c48e296
Russ Poetker <rpoetke1@jh.edu>
2024-02-21 09:51:28 -0500

    Disable spring cloud aws s3 in tests by default

--- pass-deposit-services/deposit-core/src/test/java/org/eclipse/pass/deposit/config/spring/RepositoriesConfigS3IT.java
+++ pass-deposit-services/deposit-core/src/test/java/org/eclipse/pass/deposit/config/spring/RepositoriesConfigS3IT.java
@@ -46,6 +46,7 @@ import org.testcontainers.utility.DockerImageName;
 @SpringBootTest(classes = DepositApp.class)
 @TestPropertySource(properties = {
     "spring.jms.listener.auto-startup=false",
+    "spring.cloud.aws.s3.enabled=true",
     "pass.deposit.repository.configuration=s3://test-bucket/s3-test-repositories.json"
 })
 @Testcontainers
