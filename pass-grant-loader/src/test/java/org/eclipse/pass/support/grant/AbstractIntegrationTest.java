1d32ef1576cd3452a7c69a45d8ba3363f0a457a4
Russ Poetker <rpoetke1@jh.edu>
2024-02-26 16:18:38 -0500

    Fix tests

--- pass-grant-loader/src/test/java/org/eclipse/pass/support/grant/AbstractIntegrationTest.java
+++ pass-grant-loader/src/test/java/org/eclipse/pass/support/grant/AbstractIntegrationTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 Johns Hopkins University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.eclipse.pass.support.grant;
 
 import java.io.FileReader;
@@ -5,7 +20,7 @@ import java.io.FileReader;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.eclipse.pass.support.client.PassClient;
-import org.junit.jupiter.api.BeforeEach;
+import org.eclipse.pass.support.grant.data.GrantConnector;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,6 +33,9 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+/**
+ * @author Russ Poetker (rpoetke1@jh.edu)
+ */
 @SpringBootTest(classes = GrantLoaderCLI.class)
 @TestPropertySource("classpath:test-application.properties")
 @Testcontainers
@@ -45,7 +63,8 @@ public abstract class AbstractIntegrationTest {
         .withExposedPorts(8080);
 
     @Autowired protected PassClient passClient;
-
+    @Autowired protected GrantLoaderApp grantLoaderApp;
+    @MockBean protected GrantConnector grantConnector;
     @MockBean protected GrantLoaderCLIRunner grantLoaderCLIRunner;
 
     @DynamicPropertySource
