555cc5bcd9ea24c6ec8a8c18d576bb886a3db6ef
Russ Poetker <rpoetke1@jh.edu>
2024-02-27 09:50:18 -0500

    Update docker build

--- pass-grant-loader/entrypoint.sh
+++ pass-grant-loader/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -DAPP_HOME=$APP_HOME_ENV -Dpass.core.url=$PASS_CORE_URL -Dpass.core.user=$PASS_CORE_USER -Dpass.core.password=$PASS_CORE_PASSWORD -jar jhu-grant-loader-exec.jar "$@"
+java -jar jhu-grant-loader-exec.jar "$@"
