fa69eecc082b2ef1a841fdf7722c6903a19d8023
Russ Poetker <rpoetke1@jh.edu>
2024-02-27 10:22:30 -0500

    Add cli test

---
+++ pass-grant-loader/src/test/java/org/eclipse/pass/support/grant/GrantLoaderCLITest.java
@@ -0,0 +1,46 @@
+/*
+ *
+ *  * Copyright 2024 Johns Hopkins University
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+package org.eclipse.pass.support.grant;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+
+/**
+ * @author Russ Poetker (rpoetke1@jh.edu)
+ */
+@SpringBootTest(classes = GrantLoaderCLI.class,
+    args = {"--startDateTime=2024-01-01T00:00:00", "-awardEndDate=01/01/2025", "-action=pull", "test-pull-file.csv"})
+@TestPropertySource("classpath:test-application.properties")
+public class GrantLoaderCLITest {
+
+    @MockBean GrantLoaderApp grantLoaderApp;
+
+    @Test
+    public void testHarvesterCLI() throws PassCliException {
+        // GIVEN/WHEN
+        // THEN
+        verify(grantLoaderApp).run(eq("2024-01-01T00:00:00"), eq("01/01/2025"), eq("grant"),
+            eq("pull"), eq("test-pull-file.csv"), eq(null), eq(false));
+    }
+
+}
