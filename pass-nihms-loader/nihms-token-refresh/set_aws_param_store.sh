e96035988cc991a53c09113b6daaee3af8a00605
Russ Poetker <rpoetke1@jh.edu>
2024-10-14 15:37:16 -0400

    Externalize nihms api token param name

--- pass-nihms-loader/nihms-token-refresh/set_aws_param_store.sh
+++ pass-nihms-loader/nihms-token-refresh/set_aws_param_store.sh
@@ -3,7 +3,7 @@
 export AWS_PAGER=""
 
 aws ssm put-parameter \
-    --name "NIHMS_API_TOKEN" \
+    --name $NIHMS_API_TOKEN_PARAM_NAME \
     --type "SecureString" \
     --value file://$NIHMS_OUTFILE \
     --overwrite
