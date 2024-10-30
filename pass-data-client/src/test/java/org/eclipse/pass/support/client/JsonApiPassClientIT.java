6415ab5fb3e5c6a82a56488516f4e0702678cb66
Russ Poetker <rpoetke1@jh.edu>
2024-10-30 12:59:07 -0400

    Use pass-core-test-config artifact from pass-core for ITs

--- pass-data-client/src/test/java/org/eclipse/pass/support/client/JsonApiPassClientIT.java
+++ pass-data-client/src/test/java/org/eclipse/pass/support/client/JsonApiPassClientIT.java
@@ -100,7 +100,7 @@ public class JsonApiPassClientIT {
 
         System.setProperty("pass.core.url", "http://localhost:8080");
         System.setProperty("pass.core.user", "backend");
-        System.setProperty("pass.core.password", "backend");
+        System.setProperty("pass.core.password", "moo");
 
         Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
         root.setLevel(Level.WARN);
@@ -109,10 +109,15 @@ public class JsonApiPassClientIT {
     @Container
     private static final GenericContainer<?> PASS_CORE_CONTAINER = new GenericContainer<>(PASS_CORE_IMG)
         .withCopyFileToContainer(
-            MountableFile.forHostPath("../pass-core-test-config/"),
-            "/tmp/pass-core-test-config/"
+            MountableFile.forClasspathResource("/saml2/"),
+            "/tmp/saml2/"
         )
-        .withEnv("PASS_CORE_JAVA_OPTS", "-Dspring.config.import=file:/tmp/pass-core-test-config/application-test.yml")
+        .withCopyFileToContainer(
+            MountableFile.forClasspathResource("/application-test.yml"),
+            "/tmp/application-test.yml"
+        )
+        .withEnv("PASS_SAML_PATH", "file:/tmp/")
+        .withEnv("PASS_CORE_JAVA_OPTS", "-Dspring.config.import=file:/tmp/application-test.yml")
         .withCreateContainerCmdModifier(cmd -> {
             cmd.getHostConfig().withPortBindings(new PortBinding(Ports.Binding.bindPort(8080),
                     new ExposedPort(8080)));
